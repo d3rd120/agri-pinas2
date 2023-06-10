f5d796166b5a29b0adb6eda1af8d6cd7daaf5dd8
Romeo P. London III <romeothethird456@gmail.com>
2023-06-10 19:16:17 +0800

    installed some packages, fixed farmernavigation

--- src/pages/FarmerPage/farmerCommunityForum.jsx
+++ src/pages/FarmerPage/farmerCommunityForum.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import FarmerNavigation from '../../components/farmerPageNavigation';
 
-const FarmerDashboard = () => {
+const FarmerCommunityForum = () => {
 
     return (
         <>
         <FarmerNavigation />
-        <h1>This is Farmer Dashboard</h1>
+        <h1>This is Farmer Community</h1>
         </>
     );
 };
 
-export default FarmerDashboard;
+export default FarmerCommunityForum;
