d2eb7161241ab4b4caa542a717e85ed90fadbb93
Romeo P. London III <romeothethird456@gmail.com>
2023-06-28 23:08:45 +0800

    eto arrianne

--- src/components/buyerProfileComponent.jsx
+++ src/components/buyerProfileComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../css/BuyerPage/buyerCommunityForumPost.css';
+import "../css/Components/farmerProfileComponent.css";
 import BuyerProfileNav from '../components/buyerProfileNav';
 import ProfileVector1 from '../img/profileVector1.png';
 import BuyerTopNav from '../components/buyerTopNav';
@@ -7,7 +7,7 @@ import BuyerTopNav from '../components/buyerTopNav';
 const BuyerProfile = () => {
   const [name, setName] = useState('Jenkins Mesina');
   const [phoneNumber, setPhoneNumber] = useState('0987654321');
-  const [email, setEmail] = useState('jenkins123@gmail.com');
+  const [email, setEmail] = useState('jenkins123@gmail.com ');
   const [dateOfBirth, setDateOfBirth] = useState('2000-01-01');
 
   const handleUserInfoChange = (userInfo) => {
@@ -25,109 +25,38 @@ const BuyerProfile = () => {
   };
 
   return (
-    <div className="buyerCommunityForumComponentFullPost">
+    <div className="farmerProfileComponent">
       <BuyerProfileNav
         onUserInfoChange={handleUserInfoChange}
         name={name}
         phoneNumber={phoneNumber}
         email={email}
         dateOfBirth={dateOfBirth}
-      />
-      <div className="farmerMarketplaceComponentMainPanel">
-        <BuyerTopNav />
-        <div className="farmerCommunityForumComponentTopSection">
-          <div className="farmerCommunityForumComponentMainText1">
-            <b className="farmerCommunityForumComponentMainText2">
-              <p className="farmerCommunityForumComponentBlankLine">My Profile</p>
+      />     
+      <div className="farmerProfileComponentMainPanel">
+        <BuyerTopNav /> 
+        <div className="farmerProfileComponentTopSection">
+          <div className="farmerProfileComponentMainText1">
+            <b className="farmerProfileComponentMainText2">
+              <p className="farmerProfileComponentBlankLine">My Profile</p>
             </b>
           </div>
         </div>
-        <div className="buyeAccountComponentMiddleSection">
-          <div className="FarmerAccountComponentCardsContainer">
-            <div className="FarmerAccountComponentCardsContainerInner">
-              <a className="FarmerAccountComponentRectangleParent">
-                <img
-                  className="FarmerAccountComponentFrameItem"
-                  alt=""
-                  src={ProfileVector1}
-                />
-                <div className="FarmerAccountComponentFrameWrapper">
-                  <div className="FarmerAccountComponentFrameGroup">
-                    <div className="FarmerAccountComponentNameTextWrapper">
-                      <b className="FarmerAccountComponentNameText">{name}</b>
-                    </div>
-                    <div className="FarmerAccountComponentDetailsChild" />
-                    <div className="FarmerAccountComponentRoleWrapper">
-                      <div className="FarmerAccountComponentRole">
-                        <b>Role: </b>
-                        <span>Buyer</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div className="FarmerAccountComponentSmallCards">
-              <div className="FarmerAccountComponentSmallCardsDescription">
-                <div className="FarmerAccountComponentSmallCardsContent">
-                  <div className="FarmerAccountComponentSmallCardsHeading">
-                    <div className="buyerComponentSmallCardsDetails">
-                      <b className="FarmerAccountComponentSmallCardsCourseName">
-                        Personal Information
-                      </b>
-                      <div className="FarmerAccountComponentSmallCardsDetailsChild" />
-                    </div>
-                    <div className="FarmerAccountComponentFullDescriptionWrapper">
-                      <div className="FarmerAccountComponentFullDescription">
-                        <p className="FarmerAccountComponentBlankLine">
-                          <b className="FarmerAccountComponentCategory">Full Name: </b>
-                          <div className="name">Jenkins Mesina</div>
-                        </p>
-                        <p className="FarmerAccountComponentBlankLine">
-                          <b className="FarmerAccountComponentCategory">Birthdate: </b>
-                          <div className="bdate">1999-04-14</div>
-                        </p>
-                        <p className="FarmerAccountComponentBlankLine">
-                          <b>Email Address: </b>
-                          <div className="email">jenk*********@gmail.com</div>
-                        </p>
-                        <p className="FarmerAccountComponentBlankLine">
-                          <b className="FarmerAccountComponentCategory">Age: </b>
-                          <div className="age">24</div>
-                        </p>
-                        <p className="FarmerAccountComponentBlankLine">
-                          <b>Address: </b>
-                          <div className="address">
-                            Kristong Hari, Quezon City, Metro Manila, Metro Manila
-                          </div>
-                        </p>
-                        <p className="FarmerAccountComponentBlankLine">
-                          <b>Contact Number: </b>
-                          <div className="number">09675046713</div>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="courses-container" style={{ marginTop: '-40px' }}>
-          <div className="courseProfile">
-            <div className="course-preview1">
-              <div className="info">Manage and protect your account</div>
-              <div className="fname">Name</div>
-              <div className="name">{name}</div>
-              <div className="role">Role</div>
-              <img src={ProfileVector1} className="selectImageIcon" />
-              <div className="role1">Buyer</div>
-              <div className="email1">Email</div>
-              <div className="email">{email}</div>
-              <div className="pnumber">Phone Number</div>
-              <div className="number">{phoneNumber}</div>
-              <div className="bdaydate">Birth Date</div>
-              <div className="bdate">{formatDateOfBirth(new Date(dateOfBirth))}</div>
+        <div style={{ marginTop: "-40px" }}>
+          <div class="farmercourseProfile">
+            <div class="course-preview1">
+              <div class="farmerProfileComponentInfo">Manage and protect your account</div>
+              <div class="farmerProfileComponentFullName">Name</div>
+              <div class="farmerProfileComponentName">{name}</div>
+              <div class="farmerProfileComponentRole">Role</div>
+              <img src={ProfileVector1} class="farmerselectImageIcon" />
+              <div class="farmerProfileComponentRole2">Farmer</div>
+              <div class="farmerProfileComponentEmail1">Email</div>
+              <div class="farmerProfileComponentEmail2">{email}</div>
+              <div class="farmerProfileComponentPhoneNumber">Phone Number</div>
+              <div class="farmerProfileComponentNumber">{phoneNumber}</div>
+              <div class="farmerProfileComponentBdayDate">Birth Date</div>
+              <div class="farmerProfileComponentBirthdate">{formatDateOfBirth(new Date(dateOfBirth))}</div>
             </div>
           </div>
         </div>
