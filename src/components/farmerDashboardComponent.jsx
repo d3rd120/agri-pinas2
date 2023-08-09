3b8a079b29e18d55c8291ad650555daf15accf2f
Romeo P. London III <romeothethird456@gmail.com>
2023-08-09 14:50:15 +0800

    prop weather api

--- src/components/farmerDashboardComponent.jsx
+++ src/components/farmerDashboardComponent.jsx
@@ -6,6 +6,7 @@ import { I18nextProvider } from 'react-i18next';
 import { useTranslation } from 'react-i18next';
 import i18n from '../i18n';
 import Banner from '../img/bannerSample.png';
+import Weather from '../img/weatherAPI.png';
 
 
 
@@ -38,10 +39,13 @@ const FarmerDashboard = () => {
           <div className="farmerDashboardComponentOverview">
             <div className="farmerDashboardComponentMainText2">
             {t('farmerPageDashboardText2')}
-            </div>            
+            </div>   
+            <div className="farmerDashboardComponentCard2">           
+            <img className="farmerDashboardIcon2" alt="" src={Weather} />
+            </div>      
             <div className="farmerDashboardComponentCard">           
             <img className="farmerDashboardIcon" alt="" src={Banner} />
-            </div>             
+            </div>                   
           </div>
         </div>
       </div>
